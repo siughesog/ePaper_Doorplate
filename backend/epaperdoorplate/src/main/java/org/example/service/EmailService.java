8b95bf6c8687cce0bb5e28ddae0ce178250ac25a
James Hsu <james.hsu8328@gmail.com>
2025-11-10 19:37:34 +0800

    fix: Remove unused import in EmailService

--- backend/epaperdoorplate/src/main/java/org/example/service/EmailService.java
+++ backend/epaperdoorplate/src/main/java/org/example/service/EmailService.java
@@ -3,8 +3,6 @@ package org.example.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
 public class EmailService {
 
